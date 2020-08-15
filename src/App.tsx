dc5396f72af68bd185692521122ec540b757079c
lfujiwara <lucastfujiwara@gmail.com>
2020-08-14 22:05:32 -0300

    Save template

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="w-full min-h-screen flex justify-center items-center">
+      <span>Hello, world</span>
+    </div>
+  );
+}
+
+export default App;
